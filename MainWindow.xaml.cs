31e6bc835157b57e50fde77cafe7cdf72352f92b
Salvador <sarevalotamayo@student.rtc.edu>
2024-06-11 19:14:04 -0700

    upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,6 +19,71 @@ namespace Prog122_S24_L13_RichTextBox_Sal
         public MainWindow()
         {
             InitializeComponent();
+
+            FontSizeComboBox.SelectedIndex = 2;
+        
+        
+        } // MainWindow()
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            // 1. Grab the text from the title text box and the paragraph rich text box.
+            string title = TitleTextBox.Text;
+            string paragraphText = runParagraphText.Text;
+
+            // 2. Create an instance of run for each string
+            Run runTitle = new Run(title);
+            Run runParagraph = new Run(paragraphText);
+
+            // 3. Create 2 paragraphs, because a paragraph will go to the next line
+            Paragraph p1 = new Paragraph(runTitle);
+            Paragraph p2 = new Paragraph(runParagraph);
+
+            // 4. Create an instance of FlowDocument. add our paragraph to the .Blocks.Add()
+            FlowDocument entirePage = new FlowDocument();
+            entirePage.Blocks.Add(p1);
+            entirePage.Blocks.Add(p2);
+
+            // Grabbing the text style to apply
+            if(BoldRadioButton.IsChecked.Value)
+            {
+                entirePage.FontWeight = FontWeights.Bold;
+            }
+            else if (ItalicRadioButton.IsChecked.Value)
+            {
+                entirePage.FontStyle = FontStyles.Italic;
+            }
+            else if (UnderlineRadioButton.IsChecked.Value)
+            {
+                foreach (Paragraph item in entirePage.Blocks)
+                {
+                    item.TextDecorations = TextDecorations.Underline;
+                }
+            }
+
+
+            // MessageBox.Show(FontSizeComboBox.SelectedValue);
+            // double fontSize = double.Parse(FontSizeComboBox.SelectedValue.ToString());
+            
+            entirePage.FontSize = GetFontSize();
+            
+
+
+            // 5. Add the FlowDocument to the RichTextBox
+            FormattedRichTextBox.Document = entirePage;
+
+
         }
-    }
-}
+
+        public double GetFontSize()
+        {
+            ComboBoxItem cbi = FontSizeComboBox.SelectedItem as ComboBoxItem;
+            string content = cbi.Content.ToString();
+            double fontSize = double.Parse(content);
+            return fontSize;
+        }
+
+
+    } // class
+
+} // namespace
